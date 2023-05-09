ffcf9c0fbb09282bdbe56b273efd74e2670da095
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2023-05-09 18:22:56 -0500

    Nikola auto commit.
    Nikola version: 8.2.4

---
+++ files/posts/a-random-walker/sketch.js
@@ -0,0 +1,24 @@
+const WALKER_SKETCH_DIV = "random-walker-sketch";
+const HEIGHT = 400;
+const STEP_LIMIT = 5;
+const WHITE = 255;
+
+/** Sketch of a Random Walk */
+function random_walk(p5js){
+  let walker;
+
+  /** Initial setup of the canvas and Walker */
+  p5js.setup = function() {
+      p5js.createCanvas(
+        document.getElementById(WALKER_SKETCH_DIV).offsetWidth, HEIGHT);
+    walker = new Walker(p5js.width/2, p5js.height/2, STEP_LIMIT, p5js);
+    p5js.background(WHITE);
+  } // end seutp 
+
+  /** Draw a frame */
+  p5js.draw = function() {
+      walker.move_and_show();
+    } // end draw
+} // end random_walk
+
+new p5(random_walk, WALKER_SKETCH_DIV);
