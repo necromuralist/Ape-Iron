6b3cd2c893c8d6a021fdfda9147e9b4cb44dead7
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2023-05-11 18:00:14 -0500

    Added Noise-Based Walker Post and Lancelot Plugin

--- files/posts/a-noisy-walker/sketch.js
+++ files/posts/a-noisy-walker/sketch.js
@@ -1,16 +1,32 @@
+NOISY_WALKER_DIV = "noisy-walker";
+
+/** a sketch to make a circle-drawing walker that moves with noise */
 function noise_walk(p5js) {
   let walker;
 
-  p5js.setup = function() {
-    p5js.createCanvas(1000, 400); // creating canvas of size 640 x 240
-    walker = new NoiseWalker(p5js); // creating an instance/object of class Walker
+  /** setup the canvas, walker, and slider */
+  p5js.setup = function() {  
+    let medium_teal_blue = p5js.color(0, 89, 179, 100);
+    
+    p5js.createCanvas(
+      document.getElementById(NOISY_WALKER_DIV).offsetWidth,
+      400);
+    
+    // a slider to let the user change how much the noise is changing
+    slider = p5js.createSlider(0, 1, 0.01, 0);
+    slider.style("width", "500px");
+    
+    walker = new NoiseWalker({p5: p5js,
+                              slider: slider,
+                              fill_color:medium_teal_blue});
     p5js.background("white");
   } // end setup
-
+  
+  /** move and draw the walker */
   p5js.draw = function() {
     walker.walk();
-    walker.display();
-  }
+    walker.show_yourself();
+  } // end draw
 } // end noise_walker
 
-new p5(noise_walk, "noisy-walker")
+new p5(noise_walk, NOISY_WALKER_DIV);
