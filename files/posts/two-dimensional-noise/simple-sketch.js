5e4af4ebb069c8c064ca9290cdd9ad9f484a02c6
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2023-05-14 19:42:20 -0500

    Added static 2D noise plot post (in-progress)

---
+++ files/posts/two-dimensional-noise/simple-sketch.js
@@ -0,0 +1,28 @@
+function simple_sketch(p5) {
+  p5.setup = function() {
+    p5.createCanvas(1000, 400);
+    p5.background("white");
+  } //end setup
+
+  p5.draw = function() {
+    p5.loadPixels();
+    let offset_x = 0.0;
+
+    for (let x = 0; x < p5.width; x++) {
+      let offset_y = 0.0;
+
+      for (let y = 0; y < p5.height; y++) {
+        let brightness = p5.noise(offset_x, offset_y) * 255
+        p5.set(x, y, p5.floor(brightness));
+        offset_y += 0.01;
+      } //end y-for
+      offset_x += 0.01;
+    } // end x-for
+
+    p5.updatePixels();
+    p5.noLoop();
+  } // end draw
+}// end simple_sketch
+
+
+new p5(simple_sketch, "simple-2d-noise-graph");
