c25dda27981c49c7d03e015eb708af36b3bc5826
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2023-05-22 19:01:06 -0500

    Untangled first 2d noise sketch

--- files/posts/two-dimensional-noise/simple-sketch.js
+++ files/posts/two-dimensional-noise/simple-sketch.js
@@ -1,32 +1,45 @@
 const STATIC_NOISE_GRAPH_DIV = "simple-2d-noise-graph";
+
+/** Sketch to visualize 2D Noise using `set` */
+/*
+ * Params:
+ * - p5: A p5 instance object
+*/
 function simple_sketch(p5) {
 
+  /** Create the canvas */
   p5.setup = function() {
     p5.createCanvas(
       document.getElementById(STATIC_NOISE_GRAPH_DIV).offsetWidth,
       400);
   } //end setup
-  
 
+  /** Draw the visualization then stop the loop */
   p5.draw = function() {
-    let offset_x = 0.0;
+    let intensity;
+    let column_offset = 0.0;
+    let row_offset;
+
+    p5.loadPixels();
+
+    for (let column = 0; column < p5.width; column++) {
+      row_offset = 0.0;
+    
+      for (let row = 0; row < p5.height; row++) {
+
+        let intensity = p5.noise(column_offset, row_offset) * 255;
+        p5.set(column, row, intensity);
   
-      p5.loadPixels();
-  
-      for (let x = 0; x < p5.width; x++) {
-        let offset_y = 0.0;
-  
-        for (let y = 0; y < p5.height; y++) {
-          let intensity = p5.noise(offset_x, offset_y) * 255
-          p5.set(x, y, p5.floor(intensity));
-          offset_y += 0.01;
-        } //end y-for
-        offset_x += 0.01;
-      } // end x-for
-  
-      p5.updatePixels();
-      p5.noLoop();
-    } // end draw
+        row_offset += 0.01;
+      } //end row-for
+    
+      column_offset += 0.01;
+    } // end column-for
+
+    p5.updatePixels();
+    p5.noLoop();
+  } // end draw
+    
 }// end simple_sketch
 
 new p5(simple_sketch, STATIC_NOISE_GRAPH_DIV);
