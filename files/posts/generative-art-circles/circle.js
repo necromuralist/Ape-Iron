ae2607d4323f0e736f2512400f70274e50c6d3ff
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2023-06-05 17:45:22 -0500

    changed css to center p5 sketches

--- files/posts/generative-art-circles/circle.js
+++ files/posts/generative-art-circles/circle.js
@@ -1,32 +1,44 @@
 const CIRCLE_DIV = "hard-circle";
 
 function circle(p5){
-  const RADIUS = 100;
   const DEGREES_IN_A_CIRCLE = 360;
+  const RADIUS = 100;
+  const SMALL_DIAMETER = 10;
+  const STEP = 5;
   const TO_RADIANS = (2 * Math.PI)/ DEGREES_IN_A_CIRCLE;
+  const WIDTH = 3 * RADIUS;
   
   let center_x;
   let center_y;
-  let start = 0;
+  let slider;;
+  let start = 0
   
   p5.setup = function() {
-    p5.createCanvas(1000, 400);
+    p5.createCanvas(WIDTH, 400);
     p5.background("white");
     p5.stroke("black");
+    p5.fill("black");
     center_x = p5.width/2;
     center_y = p5.height/2;
+    slider = p5.createSlider(
+      1,
+      364,
+      2,
+      1);
+slider.style("width", "500px");
   } // end setup
 
   
   p5.draw = function() {
-    for (let angle=start; angle <= DEGREES_IN_A_CIRCLE + start; angle += 1) {
+    for (let angle=start; angle <= DEGREES_IN_A_CIRCLE + start; angle += slider.value()) {
       radians = angle * TO_RADIANS;
       x = center_x + RADIUS * Math.cos(radians);
       y = center_y + RADIUS * Math.sin(radians);
-      p5.circle(x, y, 5);
+      p5.line(center_x, center_y, x, y);
+      p5.circle(x, y, SMALL_DIAMETER);
       p5.background(255, 10);
     } // end for
-    start = (start + 5) % 360 ;
+    start = (start + STEP) % 360 ;
   } // end draw
 } // end circle
 
