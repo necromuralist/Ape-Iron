0b0da39da147e2d43949835b13f2aea3d6d16e15
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2023-08-31 20:50:21 -0500

    Initial conversion of Concentric Circles logic to a class

--- files/posts/generative-art-concentric-circles/concentric-circles.js
+++ files/posts/generative-art-concentric-circles/concentric-circles.js
@@ -1,79 +1,84 @@
 const CONCENTRIC_CIRCLES_DIV = "03d2e681-concentric-circles";
 
 class ConcentricCircles {
+  // geometry
   degrees_in_a_circle = 360;
   to_radians = (2 * Math.PI)/ this.degrees_in_a_circle;
 
-  constructor(p5, width=500, height=500, point_diameter=1, radius=5, step=10){
+  // the starting values for the circles
+  radius = 5;
+  _step = 10;
+
+  // the center of our sketch (and the circles)
+  _center_x;
+  _center_y;
+
+  // the size of the circle to draw  the circles
+  point_diameter = 1;
+  
+  constructor(p5, width, height){
     this.p5 = p5;
     this.width = width;
     this.height = height;
-    this.point_diameter=point_diameter;
-    this.radius = radius;
-    this.step = step;
   } // constructor
 
-} // Concentric Circles
+  get center_x() {
+    if (this._center_x === undefined) {
+      this._center_x = this.width/2;
+    }
+    return this._center_x;
+  }
+
+  get center_y() {
+    if (this._center_y === undefined) {
+      this._center_y = this.height/2;
+    }
+    return this._center_y;
+  }
+
+  draw() {
+    let radians, x, y;
+  
+    for (let angle = 0; angle < this.degrees_in_a_circle; angle += 1){
+        radians = angle * this.to_radians;
+        x = this.center_x + this.radius * Math.cos(radians);
+        y = this.center_y + this.radius * Math.sin(radians);
+        this.p5.circle(x, y, this.point_diameter);
+    }
+  
+    this.radius += this.step;
+  }
+
+  get step() {
+    if (this.radius > (this.center_x - this._step) || this.radius <= 0) {
+        this._step *= -1;
+      }
+    return this._step
+  }
+
+} // Concentric Circles class
 
 function concentric_circles(p5){
-  const DEGREES_IN_A_CIRCLE = 360;
-  const TO_RADIANS = (2 * Math.PI)/ DEGREES_IN_A_CIRCLE;
+  // the size of the canvas and the color of the circles
   const WIDTH = 500;
   const HEIGHT = WIDTH;
-  const POINT_DIAMETER = 1;
-  
-  let radius = 5;
-  let step = 10;
-  let center_x;
-  let center_y;
+  const POINT_COLOR = "RoyalBlue";
 
+  const circles = new ConcentricCircles(p5, WIDTH, HEIGHT);
+  
   p5.setup = function(){
     p5.createCanvas(WIDTH, HEIGHT);
     p5.background("white");
-    p5.stroke("black");
-    p5.fill("black");
-    center_x = WIDTH/2;
-    center_y = HEIGHT/2;
+    p5.stroke(POINT_COLOR);
+    p5.fill(POINT_COLOR);
     p5.frameRate(10);
-  }
-  
+  } // end setup
+
   p5.draw = function() {
-    let radians;
-    let x;
-    let y;
-  
-    for (let angle = 0; angle < DEGREES_IN_A_CIRCLE; angle += 1){
-      radians = angle * TO_RADIANS;
-      x = center_x + radius * Math.cos(radians);
-      y = center_y + radius * Math.sin(radians);
-      p5.circle(x, y, POINT_DIAMETER);
-    } // end for
-  
-
-    if (radius > (WIDTH/2 - step) || radius <= 0) {
-      step *= -1;
-    }
-
-    radius += step;
+    circles.draw();
     p5.background(255, 75);
-
-
   }// end draw
-} // end concentric_circles
 
-class TestThis {
-  x = 5;
+} // end concentric_circles sketch function
 
-  constructor() {
-  }
-
-  test(){
-    console.log(this.x);
-  }
-}
-
-const t = new TestThis();
-t.test();
-const c = new ConcentricCircles();
-c.test();
-let concentric_p5 = new p5(concentric_circles, CONCENTRIC_CIRCLES_DIV);
+new p5(concentric_circles, CONCENTRIC_CIRCLES_DIV);
