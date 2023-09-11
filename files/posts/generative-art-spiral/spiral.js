4a08f0f093a7a995afb1fe189d8c4558c3b3e0cc
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2023-09-10 20:20:47 -0500

    Broke up the spiral post tangle into noweb sections

--- files/posts/generative-art-spiral/spiral.js
+++ files/posts/generative-art-spiral/spiral.js
@@ -1,7 +1,7 @@
 const SPIRAL_DIV = "0a168ba9-spiral";
 
 class Spiralizer {
-    // geometry
+  // geometry
   degrees_in_a_circle = 360;
   to_radians = (2 * Math.PI)/ this.degrees_in_a_circle;
 
@@ -11,50 +11,38 @@ class Spiralizer {
   angle = 0;
   angle_increment = 5;
 
-  last_x;
-  last_y;
-  
-
   // the center of our sketch (and the circles)
   center_x;
   center_y;
 
   // the size of the circle to draw  the circles
   point_diameter = 1;
-  
+
   constructor(p5, center_x, center_y, maximum_radius){
     this.p5 = p5;
     this.center_x = center_x;
     this.center_y = center_y;
-    this.last_x = center_x;
-    this.last_y = center_y;
     this.maximum_radius = maximum_radius;
   } // constructor
 
   draw() {
     let radians, x, y;
-
+    
     radians = this.angle * this.to_radians;
     x = this.center_x + this.radius * Math.cos(radians);
     y = this.center_y + this.radius * Math.sin(radians);
-    //this.p5.circle(x, y, this.point_diameter);
-    this.p5.line(this.last_x, this.last_y, x, y);
-    this.last_x = x;
-    this.last_y = y;
-
+    this.p5.circle(x, y, this.point_diameter);
+  
     this.radius += this.radius_increment;
     this.angle += this.angle_increment;
-
+  
     if (this.radius >= this.maximum_radius) {
       this.radius = 1;
-      this.last_x = this.center_x;
-      this.last_y = this.center_y;
-
-      //this.angle = 0;
-      //this.p5.background("white", )
     }
   } // end draw
+
 } // spiralizer
+
 
 function spiral_sketch(p5) {
   // the size of the canvas and the color of the circles
@@ -73,7 +61,7 @@ function spiral_sketch(p5) {
 
   p5.draw = function() {
     spiralizer.draw();
-  p5.background(255, 5);
+    p5.background(255, 5);
   }// end draw
 } // spiral_sketch
 
